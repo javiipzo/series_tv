8d02b34ad8e1b926411e1d68c11afaa234b81723
javiipzo <javipozo999@gmail.com>
2023-05-17 12:19:34 +0200

    serieestv

---
+++ main.js
@@ -0,0 +1,21 @@
+let seriesPending = ['Breaking Bug', 'Game of Drones', 'Boolean 99', 'Arrested Developer'];
+let seriesWatching=[];
+seriesPending.push('Stranger Things');
+
+for (let i=0; i<seriesPending.length; i++){
+    if (seriesPending[i]=== 'Boolean 99'){
+    console.log('Found it!');
+    }
+    console.log(seriesPending[i]);
+}
+
+seriesWatching.push(seriesPending.shift());
+console.log(seriesPending.length);
+console.log(seriesWatching.length);
+for (let j=0; j===4;j++){
+    seriesWatching.push(seriesPending.shift());
+}
+seriesWatching.splice(0, seriesWatching.length);
+
+console.log(seriesPending.length);
+console.log(seriesWatching.length);
